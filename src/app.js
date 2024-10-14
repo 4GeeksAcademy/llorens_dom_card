913d4ea5f56ef1202d5f3ee8366476646b11cab7
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-10-14 20:45:48 +0000

    quitando_style

--- src/app.js
+++ src/app.js
@@ -13,34 +13,39 @@ const valores = [2, 3, 4, 5, 6, 7, 8, 9, "J", "K", "Q"];
 const getRandomElement = array =>
   array[Math.floor(Math.random() * array.length)];
 
-const pintar_valores = (valores_carta, number_container) => {
+const pintar_valores = (valores_carta, number_container, color) => {
   const valor = getRandomElement(valores_carta);
   const valor_carta = document.createElement("h1");
   valor_carta.textContent = valor;
-  valor_carta.style.fontSize = "10rem";
+  valor_carta.classList.add("valor-carta"); // Añadir clase
+  valor_carta.classList.add(color === "red" ? "rojo" : "negro"); // Añadir clase dependiendo del color
   number_container.appendChild(valor_carta);
 };
 
 const pintar_palos = (palo, color, container, rotar = false) => {
   const palo_content = document.createElement("h1");
   palo_content.textContent = palo;
-  palo_content.style.color = color;
-  palo_content.style.fontSize = "10rem";
+  palo_content.classList.add("palo-carta", color === "red" ? "rojo" : "negro"); // Añadir clases
   if (rotar) {
-    palo_content.style.transform = "rotate(180deg)";
+    palo_content.classList.add("rotado");
   }
   container.appendChild(palo_content);
 };
 
-const inicializar_palos = (array_palos, palo1_container, palo2_container) => {
+const inicializar_palos = (
+  array_palos,
+  palo1_container,
+  palo2_container,
+  number_container
+) => {
   const valor_obtenido = getRandomElement(array_palos);
   const color =
     valor_obtenido === "♦" || valor_obtenido === "♥" ? "red" : "black";
   pintar_palos(valor_obtenido, color, palo1_container);
   pintar_palos(valor_obtenido, color, palo2_container, true);
+  pintar_valores(valores, number_container, color); // Pasar el color también aquí
 };
 
 window.onload = () => {
-  inicializar_palos(arr_palos, palo1, palo2);
-  pintar_valores(valores, number);
+  inicializar_palos(arr_palos, palo1, palo2, number);
 };
